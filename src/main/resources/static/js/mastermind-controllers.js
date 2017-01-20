f14c3c87fab34d9b9f919d78fafa5232c33e471c
Dominique Bashizi <dominique@blupods.com>
2017-01-20 14:48:45 -0500

    changes made in class

--- src/main/resources/static/js/mastermind-controllers.js
+++ src/main/resources/static/js/mastermind-controllers.js
@@ -1,5 +1,16 @@
 angular.module('TIYMastermindApp', [])
-    .controller('GameController', function($scope, $http) {
-        console.log("Initializing GameController");
-    });
+    .controller(
+
+        'GameController',
+
+        function($scope, $http) {
+            console.log("Initializing GameController");
+
+            $scope.sampleFunction = function() {
+                console.log("insinde sampleFunction() ...");
+            }
+        }
+
+
+    );
 
